a69fc2903213560247a06088ce2954b9b6146bba
Hongxy09 <17853100535@163.com>
2021-07-02 21:03:21 +0800

    换个地方放笔记本，不然更新太频繁了吧

--- test.py
+++ test.py
@@ -1,3 +1,18 @@
-import tensorflow as tf
-print("tensorflow version:",tf.__version__)
-# C:/Otherapp/Anaconda/envs/tensorflowenvs/python.exe
+# import tensorflow as tf
+# print("tensorflow version:",tf.__version__)
+# # C:/Otherapp/Anaconda/envs/tensorflowenvs/python.exe
+import numpy as np
+Q = np.mat([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+D = np.diag([10, 100, 1000])
+P = Q.T
+
+q = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+p = q.T
+d = np.array([10, 100, 1000])
+
+a = np.dot(np.dot(Q, D), P)
+b = 0
+for i in range(3):
+    b += d[i]*np.dot(q[i], p[i])
+print(a)
+print(b)
