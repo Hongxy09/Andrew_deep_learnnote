d8116efe1cea359376b97a8133fc4f50df4faa85
Hongxy09 <17853100535@163.com>
2021-08-09 23:23:27 +0800

    学习pytorch基础

--- Pytorch/test.py
+++ Pytorch/test.py
@@ -1,5 +1,7 @@
 import torch
+import numpy as np
 import torchvision
-# x=torch.rand(5,3)
-# print(x)
-print(torch.__version__)
+import torch.nn as nn
+import torch.nn.functional as F
+x_ones = torch.ones(5,5)#5*5全是1的张量
+print(f"Ones Tensor: \n {x_ones} \n")
