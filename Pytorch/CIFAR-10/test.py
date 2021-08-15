d2845be85fda8981dc7d0884808829e6ed540859
Hongxy09-dell <17853100535@163.com>
2021-08-15 23:25:16 +0800

    预备优化网络结构

--- Pytorch/CIFAR-10/test.py
+++ Pytorch/CIFAR-10/test.py
@@ -1,11 +1,1 @@
-import matplotlib.pyplot as plt
-import numpy as np
-def fun():
-    set='train'
-    # a=np.random.randint(0,50, 49, dtype='I')  #整数     
-    # b=list(a)
-    # print(len(a))
-    # return 0
-    accuracy=79
-    print('Accuracy for %5s network: %d %%' % (set,accuracy))
-fun()
+print("hello")
